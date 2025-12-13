1fdd99c74778a7a049c77ad49e6ecdbab64ae511
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-12-13 18:52:04 +0600

    extra section added

--- src/components/ExtraSection.jsx
+++ src/components/ExtraSection.jsx
@@ -4,6 +4,7 @@ const ExtraSection = () => {
   return (
     <>
         <section className="py-20 bg-base-100">
+
         <div className="container mx-auto px-6 text-center">
           <h2 className="text-4xl font-bold mb-8">Why ContestHub?</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto">
@@ -24,6 +25,7 @@ const ExtraSection = () => {
             </div>
           </div>
         </div>
+        
       </section>
     </>
   )
